43dc5c4223df87317e8f496e231636f8b94f5ce4
Slavek Kabrda <bkabrda@redhat.com>
2024-07-31 15:17:06 +0200

    Remove debug, pin cosign to 2.2.4 for now

--- vm-testing/test/test-sign-blob.sh
+++ vm-testing/test/test-sign-blob.sh
@@ -41,7 +41,4 @@ echo "testing" > to-sign
 cosign --verbose sign-blob to-sign --bundle signed.bundle --identity-token="${TOKEN}" --timestamp-server-url="${COSIGN_TSA_URL}" --rfc3161-timestamp=timestamp.txt
 
 curl "${COSIGN_TSA_URL}"/certchain > tsa_chain.pem
-echo "start"
-cat tsa_chain.pem
-echo "end"
 cosign --verbose verify-blob --certificate-identity="${USERNAME}"@redhat.com --bundle signed.bundle to-sign --timestamp-certificate-chain=tsa_chain.pem --rfc3161-timestamp=timestamp.txt
